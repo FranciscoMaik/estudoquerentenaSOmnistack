fe09e9c7584ba6123872dbd46381a7f381bb7c4f
FranciscoMaik <chiconunes3081@gmail.com>
2020-04-13 04:49:00 -0300

    Mobile integardo

---
+++ mobile/App.js
@@ -0,0 +1,18 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+import React from 'react';
+import Routes from './src/routes';
+
+
+//praticamente utilizar Div para tudo
+//Teg View e Text
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
+
+
+
+
